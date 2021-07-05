9b99edc52f40d66fa90ee7125d6ad07777538037
jimmy <jimmy5217123@gmail.com>
2021-07-05 16:47:57 +0800

    api post test

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'front/dist/index.html'))
 })
 
+app.post('/test', (req, res) => {
+  res.json({
+    aaa: '3'
+  })
+})
+
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`)
 })
