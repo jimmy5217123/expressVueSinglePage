b220b8e2384eda8b0fe1b54d3759eddc4a9b71e8
jimmy5217123 <66046776+jimmy5217123@users.noreply.github.com>
2021-07-24 12:39:22 +0800

    init

--- client/src/views/Product.vue
+++ client/src/views/Product.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
-    <div style="display:flex">
-      <div v-for="(i, idx) in setProduct.data" :key="idx" style="width:250px">
-        <h3>{{i.setName}}</h3>
-        <img :src="i.setImage" style="width:200px" @click="aaa(i.setId)">
+    <div style="width">
+      <h2>bangTon</h2>
+    </div>
+    <div class="flexbox">
+      <div v-for="(i, idx) in setProduct.data" :key="idx">
+        <h3 style="margin:0">{{i.setName}}</h3>
+        <img :src="i.setImage" style="width:200px; height:180px">
+        <div>數量: <input name="setHowMany" :value="0" type="number"></div>
+        <div style="margin-top:10px">
+          <button @click="plusInSaleCar(idx)">加入購物車</button>
+        </div>
       </div>
     </div>
   </div>
@@ -24,8 +31,9 @@ export default {
   computed: {
   },
   methods: {
-    aaa (id) {
-      console.log(id)
+    plusInSaleCar (idx) {
+      const num = document.getElementsByName('setHowMany')[idx].value
+      console.log(num)
     }
   },
   async mounted () {
@@ -33,7 +41,22 @@ export default {
     this.singleProduct = await axios.post('/getSingleProduct')
     this.otherProduct = await axios.post('/getOtherProduct')
     const joinTest = await axios.post('/joinTest')
+    console.log(this.setProduct)
     console.log(joinTest)
   }
 }
 </script>
+
+<style scoped>
+  .flexbox{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    /* max-width: 1400px; */
+    /* margin: 0 auto; */
+  }
+  .flexbox > div {
+    /* margin: 10px; */
+    width: 250px;
+  }
+</style>
